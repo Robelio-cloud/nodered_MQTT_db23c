9355b1b1a13063bc3ed9a9182756d07219e5b6bc
Robelio-cloud <rm556786@fiap.com.br>
2025-06-17 15:17:27 -0300

    createuser.sql from nodered to C##nodered multitenant mode

--- createuser.sql
+++ createuser.sql
@@ -1,21 +1,21 @@
 -- Criação do usuário nodered com senha nodered
-CREATE USER nodered IDENTIFIED BY nodered;
+CREATE USER C##nodered IDENTIFIED BY nodered;
 
 -- Concessões de permissões básicas
-GRANT CREATE SESSION TO nodered;
-GRANT CREATE TABLE TO nodered;
+GRANT CREATE SESSION TO C##nodered;
+GRANT CREATE TABLE TO C##nodered;
 
 -- Permitir uso de espaço em tablespace USERS
-ALTER USER nodered QUOTA UNLIMITED ON USERS;
+ALTER USER C##nodered QUOTA UNLIMITED ON USERS;
 
 -- Tabela genérica para testes
-CREATE TABLE nodered.nodered (
+CREATE TABLE C##nodered.nodered (
     col1 VARCHAR2(500),
     col2 VARCHAR2(500)
 );
 
 -- Tabela principal de sensores
-CREATE TABLE nodered.sensor_data (
+CREATE TABLE C##nodered.sensor_data (
     id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     vibration NUMBER,
     temperature NUMBER,
@@ -26,8 +26,8 @@ CREATE TABLE nodered.sensor_data (
 );
 
 -- Descomente para testes manuais:
--- INSERT INTO nodered.sensor_data (vibration, temperature, rpm, current, level) VALUES (0.98, 34.5, 1200, 4.2, 75.0);
--- SELECT COUNT(*) FROM nodered.sensor_data;
+-- INSERT INTO C##nodered.sensor_data (vibration, temperature, rpm, current, level) VALUES (0.98, 34.5, 1200, 4.2, 75.0);
+-- SELECT COUNT(*) FROM C##nodered.sensor_data;
 
 -- Finaliza a sessão
 EXIT;
